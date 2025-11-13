169abfc1d32cd77d789af9666725407a8a4e3081
fannulain <egord201717@gmail.com>
2025-11-13 19:46:46 +0200

    Reorgonized files + exe + VS proj + code
    Reorgonized files into github folder. Now you can see code of project, full VS project and Lab1 Program.exe that can be downloaded and executed.

---
+++ Lab1/Lab1 VS Project/Source.cpp
@@ -0,0 +1,220 @@
+#include <glfw3.h>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <limits>
+
+float creatureX = 0.0f;
+float creatureY = 0.0f;
+const float moveSpeed = 0.005f;
+
+//"границі" нашої фігури
+float figureMinX = std::numeric_limits<float>::max();
+float figureMaxX = std::numeric_limits<float>::lowest();
+float figureMinY = std::numeric_limits<float>::max();
+float figureMaxY = std::numeric_limits<float>::lowest();
+
+struct Point
+{
+    float x, y;
+};
+
+struct Shape
+{
+    GLenum type;
+    std::vector<Point> vertices;
+};
+
+//наш "фламінго" у примітивах
+std::vector<Shape> pictureToDraw = {
+    {GL_TRIANGLES, {{0.0f, -0.3f}, {0.1f, -0.3f}, {0.1f, -0.2f}}},
+    {GL_TRIANGLES, {{0.05f, -0.25f}, {0.05f, -0.05f}, {0.15f, -0.15f}}},
+    {GL_TRIANGLES, {{0.1f, -0.1f}, {0.1f, 0.1f}, {-0.1f, 0.1f}}},
+    {GL_TRIANGLES, {{-0.1f, 0.1f}, {0.2f, 0.1f}, {0.05f, 0.25f}}},
+    {GL_QUADS, {{-0.1f, 0.1f}, {-0.17f, 0.17f}, {-0.1f, 0.24f}, {-0.03f, 0.17f}}},
+    {GL_QUADS, {{-0.1f, 0.24f}, {-0.1f, 0.37f}, {-0.03f, 0.3f}, {-0.03f, 0.17f}}},
+    {GL_TRIANGLES, {{-0.1f, 0.37f}, {-0.1f, 0.27f}, {-0.2f, 0.27f}}}
+};
+
+//малювання примітивів + обводки
+void drawFigure(const std::vector<Shape>& figureDetails)
+{
+    //товщина обводки
+    glLineWidth(1.0f);
+
+    //примітиви
+    glColor3f(1.0f, 0.6f, 0.2f);
+    for (const Shape& shape : figureDetails)
+    {
+        glBegin(shape.type);
+        for (const Point& vertex : shape.vertices)
+        {
+            glVertex2f(vertex.x, vertex.y);
+        }
+        glEnd();
+    }
+
+    //обводка
+    glColor3f(0.0f, 0.0f, 0.0f);
+    for (const Shape& shape : figureDetails)
+    {
+        glBegin(GL_LINE_LOOP);
+        for (const Point& vertex : shape.vertices)
+        {
+            glVertex2f(vertex.x, vertex.y);
+        }
+        glEnd();
+    }
+}
+
+//обрахунок "границь" нашої фігури
+void calculateBoundingBox(const std::vector<Shape>& figureDetails)
+{
+    for (const Shape& shape : figureDetails)
+    {
+        for (const Point& vertex : shape.vertices)
+        {
+            figureMinX = std::min(figureMinX, vertex.x);
+            figureMaxX = std::max(figureMaxX, vertex.x);
+            figureMinY = std::min(figureMinY, vertex.y);
+            figureMaxY = std::max(figureMaxY, vertex.y);
+        }
+    }
+}
+
+//обробка натискання клавіш
+void processInput(GLFWwindow* window)
+{
+    //межі екрану
+    const float screenEdgeLeft = -1.0f;
+    const float screenEdgeRight = 1.0f;
+    const float screenEdgeBottom = -1.0f;
+    const float screenEdgeTop = 1.0f;
+
+    //рух вгору
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+    {
+        float nextY = creatureY + moveSpeed;
+        //перевірка на перетин верхньої границі
+        if (nextY + figureMaxY < screenEdgeTop)
+        {
+            creatureY = nextY;
+        }
+    }
+
+    //рух вниз
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+    {
+        float nextY = creatureY - moveSpeed;
+        //перевірка на перетин нижньої границі
+        if (nextY + figureMinY > screenEdgeBottom)
+        {
+            creatureY = nextY;
+        }
+    }
+
+    //рух вліво
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+    {
+        float nextX = creatureX - moveSpeed;
+        //перевірка на перетин лівої границі
+        if (nextX + figureMinX > screenEdgeLeft)
+        {
+            creatureX = nextX;
+        }
+    }
+
+    //рух вправо
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+    {
+        float nextX = creatureX + moveSpeed;
+        //перевірка на перетин правої границі
+        if (nextX + figureMaxX < screenEdgeRight)
+        {
+            creatureX = nextX;
+        }
+    }
+
+    //закриття вікна
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    //ініціалізація glfw
+    if (!glfwInit())
+    {
+        std::cerr << "Error with GLFW initialization!" << std::endl;
+        return -1;
+    }
+
+    //створення вікна
+    window = glfwCreateWindow(800, 800, "Lab1", NULL, NULL);
+    if (!window)
+    {
+        std::cerr << "Error with creating GLFW window!" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    //кажемо, щоб всі наступні операції виконувались для нашого вікна
+    glfwMakeContextCurrent(window);
+
+    //згладжування ліній
+    glEnable(GL_LINE_SMOOTH);
+    //перевага якості над швидкістю згладжування
+    glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
+
+    //перетворення координат на пікселі
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+
+    //переходимо до налаштування "камери"
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    //встановлення меж екрану у внутрішніх координатах [-1; 1]
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+
+    //перестаємо налаштовувати камеру та повертаємось до "малювання"
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    //обрахунок границь фігури
+    calculateBoundingBox(pictureToDraw);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        //колір фону
+        glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //обробка натискань
+        processInput(window);
+
+        //повертає "курсор" малювання на (0;0)
+        glLoadIdentity();
+
+        //застосування зсуву
+        glTranslatef(creatureX, creatureY, 0.0f);
+
+        //будуємо фігури
+        drawFigure(pictureToDraw);
+
+        //зміна місцями "прихованого" кадру та "видимого" кадру
+        glfwSwapBuffers(window);
+
+        //перевіряємо події
+        glfwPollEvents();
+    }
+
+    //завершення роботи
+    glfwTerminate();
+    return 0;
+}
